afa50505057a30e4812b5756409a501eeb09d922
AVWORLD\rob9575 <rsantos@esri.com>
2018-03-16 08:29:42 -0500

    Simple Routing

--- Chap1And2Intro/helloWorld.js
+++ Chap1And2Intro/helloWorld.js
@@ -1,10 +1,30 @@
 var http = require ('http');
 
 http.createServer(function(req, res) {
-    res.writeHead(200, {
-        'ContentType' : 'text/plain'
-    });
-    res.end('Hello World!');
+    //normalize url by removing querystring, optional trailing slash, and making it lower case
+    var path = req.url.replace('/\/?(?:\?.*)?$/', '').toLowerCase();
+    switch(path) {
+        case '/':
+            res.writeHead(200, {
+                'ContentType' : 'text/plain'
+            });
+            res.end('Homepage');
+            break;
+        case '/about':
+            res.writeHead(200, {
+                'ContentType' : 'text/plain'
+            });
+            res.end('About');
+            break;
+        default:
+            res.writeHead(404, {
+                'ContentType' : 'text/plain'
+            });
+            res.end('Page not found');
+            break;
+    }
+    
+    
 }).listen(3000);
 
 console.log('Server started on localhost:3000');
