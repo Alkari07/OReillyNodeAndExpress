b425bb09a10f504980e387c877260eaa74c15ff4
AVWORLD\rob9575 <rsantos@esri.com>
2018-03-16 08:43:07 -0500

    Serving static resources

--- Chap1And2Intro/helloWorld.js
+++ Chap1And2Intro/helloWorld.js
@@ -1,30 +1,44 @@
 var http = require ('http');
+var fs = require('fs');
+
+function serveStaticFile(res, path, contentType, responseCode) {
+    if(!responseCode) {
+        responseCode =200;
+    }
+    //async read file 
+    //__dirname resolves to the directory the script is running in
+    fs.readFile(__dirname + path, function(err, data) {
+        if (err) {
+            res.writeHead(500, {
+                'Content-Type': 'text/plain'
+            });
+            res.end('500 - Internal Error');
+        } else {
+            res.writeHead(responseCode, {
+                'Content-Type': 'contentType'
+            });
+            res.end(data);
+        }
+    })
+}
 
 http.createServer(function(req, res) {
     //normalize url by removing querystring, optional trailing slash, and making it lower case
     var path = req.url.replace('/\/?(?:\?.*)?$/', '').toLowerCase();
     switch(path) {
         case '/':
-            res.writeHead(200, {
-                'ContentType' : 'text/plain'
-            });
-            res.end('Homepage');
+            serveStaticFile(res, '/public/home.html', 'text/html');
             break;
         case '/about':
-            res.writeHead(200, {
-                'ContentType' : 'text/plain'
-            });
-            res.end('About');
+            serveStaticFile(res, '/public/about.html', 'text/html');
+            break;
+        case '/img/logo.jpg':
+            serveStaticFile(res, '/public/img/icon.png');
             break;
         default:
-            res.writeHead(404, {
-                'ContentType' : 'text/plain'
-            });
-            res.end('Page not found');
+            serveStaticFile(res, '/public/404.html', 'text/html', 404);
             break;
     }
     
     
 }).listen(3000);
-
-console.log('Server started on localhost:3000');
