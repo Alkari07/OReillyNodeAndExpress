ac97a2f0e94bbe3f44eaa0133f9a67514cea18fc
AVWORLD\rob9575 <rsantos@esri.com>
2018-05-16 09:02:13 -0500

    Added samples of different types of routing

--- Chap6RequestAndResponse/meadowlark.js
+++ Chap6RequestAndResponse/meadowlark.js
@@ -39,13 +39,28 @@ app.use(function(req,res,next) {
  * app.VERB ignores trailing slashes, things after ? for query strings, etc.  it just works
  */
 app.get('/', function(req, res) {
+    //render is what renders a whole page and uses template engines and other advanced functionality.  this is used most often.  
     res.render('home');
 });
-app.get('/about', function(req, res) {
-    //res.type('text/html');
+app.get('/test', function(req, res) {
+    //send sends the content explicitly, such as raw text.  Not usually used since not template processing is done.
+    res.type('text/plain');
+    res.send('this is a test');
+});
+app.get('/greeting', function(req, res) {
+    //passing context to a view
     res.render('about', {
-        fortune: fortune.getFortune(),
-        pageTestScript: '/qa/tests-about.js'
+        message: 'welcome',
+        style: req.query.style,
+        userid: req.cookies.userid,
+        username: req.session.username,
+    });
+});
+
+//the layout file views/layouts/custom.handlebars would be used here
+app.get('/custom-layout', function(req, res) {
+    res.render('custom-layout', {
+        layout: 'custom'
     });
 });
 
@@ -59,6 +74,87 @@ app.get('/tours/request-group-rate', function(req, res) {
     res.render('tours/request-group-rate');
 });
 
+//Basic form processing (from POST)
+//body-parser middleware must be linked in
+app.post('/process-contact', function(req, res) {
+    console.log('Received contact from ' + req.body.name + '<' + req.body.email + '>');
+    try {
+        //save to a database goes here
+        //is this AJAX?  if so, do not redirect, return success JSON
+        return res.xhr ? res.render({success: true}) : res.redirect(303, '/thank-you');
+    } catch(ex) {
+        return res.xhr ? res.render({error: 'Databae error.'}) : res.redirect(303, '/database-error');
+    }
+    
+});
+
+/**
+ * Basic APIs
+ */
+
+ //simple GET endpoint returning only JSON
+ app.get('/api/tours-simple', function(req, res) {
+    var tours = {};
+    tours.someEvent = 'San Francisco';
+    tours.someDate = new Date();
+    res.json(tours);
+ });
+
+ //this example uses the the res.format method to respond according to client preferences
+ app.get('/api/tours', function(req, res) {
+    var toursXml = 'some xml';
+    var toursText = 'some text';
+    res.format({
+        'application/json': function() {
+            res.json(tours);
+        },
+        'application/xml': function() {
+            res.type('application/xml');
+            res.send(toursXml);
+        },
+        'text/xml': function() {
+            res.type('text/xml');
+            res.send(toursXml);
+        },
+        'text/plain': function() {
+            res.type('text/plain');
+            res.send(toursXml);
+        }
+    });
+ });
+
+ //this example of a PUT to update uses an id field in the param path
+ app.put('/api/tour/:id', function(req, res) {
+    var p = tours.filter(function(p) {
+        return p.id ===req.params.id;
+    })[0];
+    if (p) {
+        //update fields are contained in the query string
+        if (req.query.name) p.name = req.query.name;
+        if (req.query.price) p.price = req.query.price;
+        res.json({success: true});
+    } else {
+        res.json({
+            error: "no such tour exists;"
+        });
+    }
+ });
+
+ //This example shows DEL removing a record
+ app.delete('api/tour/:id', function(req, res) {
+    var i;
+    for (var i=tours.length-1; i>=0; i--) {
+        if (tours[i].id==req.params.id) break;
+    }
+    if (i>=0) {
+        tours.splice(i, 1);
+        res.json({success:true});
+    } else {
+        res.json({
+            error: "no such tour exists;"
+        });
+    }
+ });
 /**
  * Handlers
  * express has more robust methods and attributes for handling responses and requests
@@ -73,6 +169,7 @@ app.use(function(req, res) {
 });
 
 //custom 500 page
+//the next function must be included for epress to recognize this as an error handler, even if it's not used
 app.use(function(err, req, res, next) {
     console.error(err.stack);
     res.status(500);
